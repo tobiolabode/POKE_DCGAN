114cb867e0411d14c296b9e5b90b0189c9c57889
tobiolabode <tobi@tobiolabode.com>
2018-09-02 19:13:14 +0100

    Preprocessing

--- main.py
+++ main.py
@@ -11,3 +11,17 @@ def _parse_function(filename):
     image_decoded = tf.image.decode_jpeg(image_string)
     image_resized = tf.image.resize_images(image_decoded, [28, 28])
     return image_resized
+
+
+dataset = tf.data.Dataset.from_tensor_slices((mylist))
+dataset = dataset.map(_parse_function)
+Bdataset = dataset.batch(10)
+
+iterator = Bdataset.make_one_shot_iterator()
+next_element = iterator.get_next()
+
+
+print(dataset)
+print(Bdataset)
+print(iterator)
+print(next_element)
