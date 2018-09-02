b43af4a1849e5ae2bf9ef660a3caf9edd4c6afec
tobiolabode <tobi@tobiolabode.com>
2018-09-02 19:13:43 +0100

    Gen model

--- main.py
+++ main.py
@@ -25,3 +25,39 @@ print(dataset)
 print(Bdataset)
 print(iterator)
 print(next_element)
+
+
+class Generator(tf.keras.Model):
+    def __init__(self):
+        super(Generator, self).__init__()
+        self.fc1 = tf.keras.layers.Dense(7 * 7 * 64, use_bias=False)
+        self.batchnorm1 = tf.keras.layers.BatchNormalization()
+
+        self.conv1 = tf.keras.layers.Conv2DTranspose(
+            64, (5, 5), strides=(1, 1), padding='same', use_bias=False)
+        self.batchnorm2 = tf.keras.layers.BatchNormalization()
+
+        self.conv2 = tf.keras.layers.Conv2DTranspose(
+            32, (5, 5), strides=(2, 2), padding='same', use_bias=False)
+        self.batchnorm3 = tf.keras.layers.BatchNormalization()
+
+        self.conv3 = tf.keras.layers.Conv2DTranspose(
+            1, (5, 5), strides=(2, 2), padding='same', use_bias=False)
+
+    def call(self, x, training=True):
+        x = self.fc1(x)
+        x = self.batchnorm1(x, training=training)
+        x = tf.nn.relu(x)
+
+        x = tf.reshape(x, shape=(-1, 7, 7, 64))
+
+        x = self.conv1(x)
+        x = self.batchnorm2(x, training=training)
+        x = tf.nn.relu(x)
+
+        x = self.conv2(x)
+        x = self.batchnorm3(x, training=training)
+        x = tf.nn.relu(x)
+
+        x = tf.nn.tanh(self.conv3(x))
+        return x
