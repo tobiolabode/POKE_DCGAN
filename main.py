f09f17a9706272a1861b93713c9a19db8eac3f6d
tobiolabode <tobi@tobiolabode.com>
2018-09-02 18:58:03 +0100

    deleted parent fuction. chage to class maybe

--- main.py
+++ main.py
@@ -3,11 +3,11 @@ import PIL
 import numpy as np
 import os
 
+mylist = os.listdir("./data")
 
-def preprocess():
-    # os.mkdir(dst)
-    def _parse_function(filename):
-        image_string = tf.read_file(filename)
-        image_decoded = tf.image.decode_jpeg(image_string)
-        image_resized = tf.image.resize_images(image_decoded, [28, 28])
-        return image_resized
+
+def _parse_function(filename):
+    image_string = tf.read_file(filename)
+    image_decoded = tf.image.decode_jpeg(image_string)
+    image_resized = tf.image.resize_images(image_decoded, [28, 28])
+    return image_resized
