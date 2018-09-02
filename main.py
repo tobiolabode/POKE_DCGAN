6789c9743256676c8a985d27d9d1672a45307181
tobiolabode <tobi@tobiolabode.com>
2018-09-02 18:32:45 +0100

    precoss function for resizing and os import

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
 import tensorflow as tf
 import PIL
 import numpy as np
+import os
+
+
+def preprocess():
+    # os.mkdir(dst)
+    for each in os.listdir(src):
+        img = PIL.imread(os.path.join(src, each))
+        img = PIL.resize(img, (256, 256))
+        PIL.imwrite(os.path.join(dst, each), img)
